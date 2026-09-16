fab72021d4c70d741027f23b22529648ce18a571
AntonioDeSantis0DD <antonio.desantis@zerodd.it>
2026-09-16 10:44:19 +0200

    aggiunge autenticazione con BCrypt e generazione JWT
    - aggiunge Spring Security a user-servis
    - configura PasswordEncoder con BCrypt
    - salva le password degli account in forma hashata
    - aggiunge LoginDTOInput e LoginDTOOutput
    - implementa AuthService e endpoint POST /api/auth/login
    - gestisce credenziali non valide con risposta 401 Unauthorized
    - evita l’esposizione della password nella risposta di login
    - aggiunge supporto JWT con generazione, lettura username, scadenza e validazione token
    - aggiorna API Gateway per instradare /api/auth/** verso user-servis
    - verificati login, credenziali errate e generazione token tramite Bruno

--- user-servis/src/main/java/com/it/userservis/security/JwtService.java
+++ user-servis/src/main/java/com/it/userservis/security/JwtService.java
@@ -3,6 +3,7 @@ package com.it.userservis.security;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+import io.jsonwebtoken.JwtException;
 
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
@@ -15,16 +16,16 @@ public class JwtService {
 
     private static final long EXPIRATION_TIME = 1000 * 60 * 60;
 
-    private SecretKey getSigngKey(){
+    private SecretKey getSigngKey() {
         return Keys.hmacShaKeyFor(
                 SECRET_KEY.getBytes(StandardCharsets.UTF_8)
         );
     }
 
-    public String generatoreToken(String username){
+    public String generatoreToken(String username) {
 
         Date now = new Date();
-        Date expiration =  new Date(
+        Date expiration = new Date(
                 now.getTime() + EXPIRATION_TIME
         );
 
@@ -33,5 +34,46 @@ public class JwtService {
                 .issuedAt(now)
                 .signWith(getSigngKey())
                 .compact();
+    }
+
+    //verifica token tramite username
+    public String extractUsername(String token) {
+        return Jwts.parser()
+                .verifyWith(getSigngKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+    //scadenza token
+    public Date extractExpiration(String token) {
+
+        return Jwts.parser()
+                .verifyWith(getSigngKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getExpiration();
+    }
+
+    public boolean isTokenExpired(String token){
+
+        return extractExpiration(token)
+                .before(new Date());
+    }
+
+    public boolean isTokenValid(String token, String username) {
+
+        try {
+            String usernameToken = extractUsername(token);
+
+            return usernameToken.equals(username) && !isTokenExpired(token);
+
+        } catch (JwtException | IllegalArgumentException e) {
+
+            return false;
+        }
+
     }
 }
