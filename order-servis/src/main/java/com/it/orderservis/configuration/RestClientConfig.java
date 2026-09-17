2b6c44cf2097da4f8c953407b47f052910b0a0c1
AntonioDeSantis0DD <antonio.desantis@zerodd.it>
2026-09-17 11:02:26 +0200

    sistemato ordinazioni

--- order-servis/src/main/java/com/it/orderservis/configuration/RestClientConfig.java
+++ order-servis/src/main/java/com/it/orderservis/configuration/RestClientConfig.java
@@ -1,62 +1,53 @@
-package com.it.orderservis.config;
+package com.it.orderservis.configuration;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.http.client.JdkClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
+import java.net.http.HttpClient;
 import java.time.Duration;
 
 @Configuration
 public class RestClientConfig {
 
-    /*
-     * Builder normale.
-     * Viene lasciato disponibile a Spring/Eureka
-     * per chiamate dirette come localhost:8761.
-     */
     @Bean
     @Primary
     public RestClient.Builder restClientBuilder() {
 
-        SimpleClientHttpRequestFactory requestFactory =
-                new SimpleClientHttpRequestFactory();
+        HttpClient httpClient = HttpClient.newBuilder()
+                .connectTimeout(Duration.ofSeconds(2))
+                .build();
 
-        requestFactory.setConnectTimeout(Duration.ofSeconds(2));
+        JdkClientHttpRequestFactory requestFactory =
+                new JdkClientHttpRequestFactory(httpClient);
+
         requestFactory.setReadTimeout(Duration.ofSeconds(5));
 
         return RestClient.builder()
                 .requestFactory(requestFactory);
     }
 
-
-    /*
-     * Builder usato dai nostri client applicativi.
-     * Risolve user-servis, product-servis,
-     * payment-service, notification-service tramite Eureka.
-     */
     @Bean
     @LoadBalanced
     public RestClient.Builder loadBalancedRestClientBuilder() {
 
-        SimpleClientHttpRequestFactory requestFactory =
-                new SimpleClientHttpRequestFactory();
+        HttpClient httpClient = HttpClient.newBuilder()
+                .connectTimeout(Duration.ofSeconds(2))
+                .build();
 
-        requestFactory.setConnectTimeout(Duration.ofSeconds(2));
+        JdkClientHttpRequestFactory requestFactory =
+                new JdkClientHttpRequestFactory(httpClient);
+
         requestFactory.setReadTimeout(Duration.ofSeconds(5));
 
         return RestClient.builder()
                 .requestFactory(requestFactory);
     }
 
-
-    /*
-     * RestClient effettivamente iniettato nei nostri
-     * UserClient, ProductClient, PaymentClient e NotificationClient.
-     */
     @Bean
     public RestClient restClient(
             @Qualifier("loadBalancedRestClientBuilder")
